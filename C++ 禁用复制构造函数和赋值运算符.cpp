80fcd5c952875781d513d80ae8ac30dbf62a0245
henry3695 <6102435@qq.com>
2022-12-02 19:13:18 +0800

    Create C++ 禁用复制构造函数和赋值运算符.cpp

---
+++ C++ 禁用复制构造函数和赋值运算符.cpp
@@ -0,0 +1,47 @@
+#include "iostream"
+using namespace std;
+
+
+class MyClass
+{
+private:
+	int a;
+public:
+	MyClass(int _a)
+	{
+		a = _a;
+		cout << "MyClass()" << endl;
+	}
+	~MyClass()
+	{
+		cout << "~MyClass()" << endl;
+	}
+
+
+	MyClass(const MyClass& t) = delete;       //禁用复制构造函数
+	/*	
+	MyClass(const MyClass& t)
+	{
+		a = t.a;
+	}
+	*/
+
+	MyClass& operator=(MyClass& mc) = delete; //禁用赋值运算符
+	/*MyClass& operator=(MyClass& mc)
+	{
+		a = mc.a;
+		mc.a = 0;
+		return *this;
+	}*/
+};
+
+int main()
+{
+	MyClass my1(1);
+	MyClass my2(2);
+	
+	//MyClass my3(my1);		    //编译通不过  禁用复制构造函数
+	//my2 = std::move(my1);   //编译通不过  禁用赋值运算符
+}
+//C++ 禁用复制构造函数和赋值运算符
+
