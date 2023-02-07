10113dcd2dca7d0e87d6a9e18f48d8bb79af398b
henry3695 <6102435@qq.com>
2023-02-07 10:38:00 +0800

    Create 编译libssh 动态库.cmd

---
+++ 编译libssh 动态库.cmd
@@ -0,0 +1,22 @@
+cmake 文档
+https://github.com/libssh2/libssh2/blob/master/docs/INSTALL_CMAKE.md
+
+
+下载代码 https://github.com/libssh2/libssh2
+
+解压
+代码目录
+D:\OpenSource\libssh2-master
+
+依次打入以下命令
+mkdir bin
+cd bin
+cmake -D=BUILD_SHARED_LIBS=ON ..
+
+
+等待生成vs项目文件
+D:\OpenSource\libssh2-master\bin
+vs 双击打开libssh2.sln  ，编译
+
+
+
